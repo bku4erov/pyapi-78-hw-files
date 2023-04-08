de574375731941b7615c7be7193b2d2835163aa8
Boris Kucherov <boris.ku4erov@gmail.com>
2023-04-08 12:48:01 +0300

    bug fix in reading cook book from file

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def load_cook_book(filename):
             for i in range(ingredients_num):
                 ingredient_info = file.readline().strip()
                 ingredient_name, quantity, measure = ingredient_info.split('|')
-                ingredients.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+                ingredients.append({'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure})
             # Add dish to the cook book
             cook_book[dish_name] = ingredients
             # Read the empty line after each dish
